4fd7c578636aabf5f28950c4a0ad23a5c37f98ab
levi <1079731010@qq.com>
2021-10-16 17:48:03 +0800

    Initial commit

---
+++ javaweb/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zq
+  Date: 2021/10/16
+  Time: 16:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+  <base href="<%=basePath%>">
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
---
+++ src/com/hello.java
@@ -0,0 +1,29 @@
+package com;
+
+import java.util.ArrayList;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        for (int i = 0; i < ; i++) {
+            String[] arr = new String[]{"a","b"};
+            for (String s : arr) {
+
+            }
+            for (int j = 0; j < arr.length; j++) {
+                String s = arr[j];
+
+            }
+            ArrayList list = new ArrayList();
+            list.add(123);
+            if (list == null) {
+
+            }
+
+            }
+
+
+        }
+
+    }
+}
